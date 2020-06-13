8cd0c35cc9336b910de50169339fc8f7fd6a6502
Han <quaint3@gmail.com>
2020-06-13 18:14:47 +0900

    [#10] fix: url 수정

--- BE/src/main/java/com/codesquad/issue/IssueController.java
+++ BE/src/main/java/com/codesquad/issue/IssueController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/issues")
+@RequestMapping("issues")
 public class IssueController {
 
   private static final Logger log = LoggerFactory.getLogger(IssueController.class);
